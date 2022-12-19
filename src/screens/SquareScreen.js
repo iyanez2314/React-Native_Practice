c9b75e56712552d59f77a70a4b4d740dcd4d5e44
Isaac Yanez <isaac231467@gmail.com>
2022-12-18 19:39:32 -0600

    changed the way the small app handles state I am now using useReducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,51 +1,64 @@
-import react, { useState } from "react";
+import react, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import ColorCounter from "../Components/ColorCounter";
 
 const COLOR_INCREMENT = 15;
 
+/**
+ *
+ * @param {*} state {red: number, green: number, blue: number}
+ * @param {*} action { colorToChnage: 'red' || 'green' || 'blue', amount: 15 || -15}
+ */
+const reducer = (state, action) => {
+  switch (action.colorToChnage) {
+    case "red":
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
+    case "green":
+      return state.green + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
+    case "blue":
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
+
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  console.log(red, green, blue);
-
-  const setColor = (color, change) => {
-    switch (color) {
-      case "red":
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-      case "green":
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        return;
-      case "blue":
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        return;
-      default:
-        return;
-    }
-  };
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+  const { red, green, blue } = state; // Object destructuring
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setColor("red", COLOR_INCREMENT)}
-        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChnage: "red", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChnage: "red", amount: -1 * COLOR_INCREMENT })
+        }
         colorTitle="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor("green", COLOR_INCREMENT)}
-        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChnage: "green", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChnage: "green", amount: -1 * COLOR_INCREMENT })
+        }
         colorTitle="Green"
       />
       <ColorCounter
-        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
-        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>
+          dispatch({ colorToChnage: "blue", amount: COLOR_INCREMENT })
+        }
+        onDecrease={() =>
+          dispatch({ colorToChnage: "blue", amount: -1 * COLOR_INCREMENT })
+        }
         colorTitle="Blue"
       />
       <View
